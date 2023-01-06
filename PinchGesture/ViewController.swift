115536a0c11f69acbe1c335da8495a01a592d1b7
doomout <winquake3@hanmail.net>
2023-01-06 17:48:32 +0900

    뷰, 아웃렛 변수 까지 구현

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -1,14 +1,8 @@
-//
-//  ViewController.swift
-//  PinchGesture
-//
-//  Created by 현구김 on 2023/01/06.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var txtPinch: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
